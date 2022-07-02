a5f11f632dc677488b98a87769cd688271ae09ce
seetmrw <101439744+seetmrw@users.noreply.github.com>
2022-07-02 23:36:22 +0900

    Delete nodejs directory

--- nodejs/syntax/comparision.js
+++
@@ -1,7 +0,0 @@
-console.log(1==1);
-console.log(1==2);
-console.log(1>2);
-console.log(1<2);
-console.log(1===1);
-name = 1 ;
-console.log(name);
--- nodejs/syntax/sync.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-
-
-console.log('A');
-fs.readFile('syntax/sample.txt', 'utf8', function(err, result){
-    console.log(result);
-});
-console.log('C');
